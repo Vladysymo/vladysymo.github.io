e2fda90f2d5538894d2417ed4e6c6b6920d1322f
Vladislav Luahovetsky <vladysymo@mail.ru>
2018-08-31 01:25:30 +0300

    1

--- portfolio/js/theme.js
+++
@@ -1,9 +0,0 @@
-if (localStorage.theme) {
-	if (localStorage.theme == 'red')
-		document.write('<link rel="stylesheet" href="css/theme-red.css">')
-	if (localStorage.theme == 'black')
-		document.write('<link rel="stylesheet" href="css/theme-black.css">')
-	//if (localStorage.theme == 'default')
-	//	if (document.querySelector('link[href="css/theme-black.css"]'))
-	//		document.querySelector('link[href="css/theme-black.css"]').remove()
-}
