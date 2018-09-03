204ec4dbc441e9a773ac9c99dd4d7dfb95f14892
Vladislav Luahovetsky <vladysymo@mail.ru>
2018-09-03 20:50:08 +0300

    1

--- portfolio/js/questions.js
+++ portfolio/js/questions.js
@@ -10,77 +10,107 @@ let next = allQ.querySelector('a.btn')
 
 //Для сохранения данных об опросе
 let Data = {
-    type : ""
+	type : "",
+	price : 0,
+	responsive : false,
+	form : false,
+	slider : false, 
+	manyscreens : false
 }
 
 //Номер вопроса
 let numberQ = 0
+//Ветка вопросов
+let branchQuestions
 //Функции
 const loadFirst = (lending)=>{
 	if (lending) {
 		Data.type = "Лендинг"
 		Data.price = 50
+		console.log("Лендинг")
 	} else {
 		Data.type = "Визитка"
 		Data.price = 40
+		console.log("Визитка")
 	}
+	nextQuestion(questionsLending)
+},
+
+loadSecond = ()=>{
+    alert('В разработке...')
+},
+
+loadThird = ()=>{
+    alert('В разработке...')
+},
+
+errChoose = ()=>{
+    alert('Выберите что-нибудь!')
+},
+
+finish = ()=>{
+	if ( Data.responsive == true ) Data.price = Data.price * 1.2
+	console.log(JSON.stringify(Data))
+},
+
+addAnswer = (id, text, price)=>{
+	let elem = document.createElement('div')
+	elem.classList.add('answer-vertical')
+	elem.innerHTML = `<input type="radio" id="r${id}" name="question"><label for="r${id}"><p class="text">${text}</p><div class="price">${price}</div></label>`
+	answers.appendChild(elem)
+},
+
+changeQuestion = (question)=>{
+	answers.innerHTML = ''
+	answers.classList.add('vertical')
+	for ( let i in question.answers ) {
+		addAnswer(i+1, question.answers[i].text, question.answers[i].price)
+	}
+	h4.innerHTML = question.h4
+	p.innerHTML = question.p
+},
+
+hide = ()=>{
+	answers.classList.add('hide')
+	h4.classList.add('hide')
+	p.classList.add('hide')
+},
+
+unhide = ()=>{
+	answers.classList.remove('hide')
+	h4.classList.remove('hide')
+	p.classList.remove('hide')
+},
+
+nextQuestion = (questionsWhat)=>{	
 	let checked = false
-	for ( let el of allQ.querySelectorAll('input[type=radio]')) {
+	for ( let el of allQ.querySelectorAll('input[type=radio]') ) {
 		if ( el.checked ) {
 			checked = true
 		}
 	}
 	if ( checked ) {
-		if ( numberQ == 1 ) {
-			if ( allQ.querySelectorAll('input[type=radio]')[0].checked ) {
-				Data.responsive = true
-			} else {
-				Data.responsive = false
-			}
-		} else if ( numberQ == 2 ) {
-			if ( allQ.querySelectorAll('input[type=radio]')[0].checked ) {
-				Data.form = true
-				Data.price += parseInt(questionsLending[numberQ-1].answers[0].price)
-			} else {
-				Data.form = false
-				Data.price += parseInt(questionsLending[numberQ-1].answers[1].price)
-			}
-		} else if ( numberQ == 3 ) {
-			if ( allQ.querySelectorAll('input[type=radio]')[0].checked ) {
-				Data.slider = true
-				Data.price += parseInt(questionsLending[numberQ-1].answers[0].price)
-			} else {
-				Data.slider = false
-				Data.price += parseInt(questionsLending[numberQ-1].answers[1].price)
-			}
-		} else if ( numberQ == 4 ) {
-			if ( allQ.querySelectorAll('input[type=radio]')[0].checked ) {
-				Data.manyscreens = false
-				Data.price += parseInt(questionsLending[numberQ-1].answers[0].price)
-			} else {
-				Data.manyscreens = true
-				Data.price += parseInt(questionsLending[numberQ-1].answers[1].price)
+		let n,
+			radios = allQ.querySelectorAll('input[type=radio]') 
+		for ( let i in radios ) {
+			if ( radios[i].checked ) {
+				n = i
 			}
 		}
+		if ( numberQ <= questionsWhat.length && numberQ > 0 ) {
+			if (questionsWhat[numberQ-1].answers[n].id) {
+				console.log(questionsWhat[numberQ-1].answers[n].id)
+				Data[questionsWhat[numberQ-1].answers[n].id] = true
+			}
+			if ( questionsWhat[numberQ-1].answers[n].price[questionsWhat[numberQ-1].answers[n].price.length - 1] == '$' )
+				Data.price += parseInt(questionsWhat[numberQ-1].answers[n].price)
+		}
 		numberQ++
-		if ( numberQ <= questionsLending.length ) {
-			answers.classList.add('hide')
-			h4.classList.add('hide')
-			p.classList.add('hide')
+		if ( numberQ <= questionsWhat.length ) {
+			hide()
 			setTimeout(()=>{
-				answers.innerHTML = ''
-				answers.classList.add('vertical')
-				for ( let i in questionsLending[numberQ-1].answers ) {
-					let elem = document.createElement('div')
-					elem.classList.add('answer-vertical')
-					elem.innerHTML = `<input type="radio" id="r${i+1}" name="question"><label for="r${i+1}"><p class="text">${questionsLending[numberQ-1].answers[i].text}</p><div class="price">${questionsLending[numberQ-1].answers[i].price}</div></label>`
-					answers.appendChild(elem)
-				}
-				h4.innerHTML = questionsLending[numberQ-1].h4
-				p.innerHTML = questionsLending[numberQ-1].p
-				answers.classList.remove('hide')
-				h4.classList.remove('hide')
-				p.classList.remove('hide')
+				changeQuestion(questionsWhat[numberQ-1])
+				unhide()
 			}, 300)
 		} else {
 			finish()
@@ -88,19 +118,6 @@ const loadFirst = (lending)=>{
 	} else {
 		errChoose()
 	}
-},
-loadSecond = ()=>{
-    alert('В разработке...')
-},
-loadThird = ()=>{
-    alert('В разработке...')
-},
-errChoose = ()=>{
-    alert('Выберите что-нибудь!')
-},
-finish = ()=>{
-	if ( Data.responsive ) Data.price *= 1.2
-	alert(JSON.stringify(Data))
 }
 
 //Вопросы
@@ -111,7 +128,8 @@ let questionsLending = [
         answers : [
             {
                 text : "С адаптивностью",
-                price : "+20%"
+				price : "+20%",
+				id : "responsive"
             },
             {
                 text : "Без адаптивности",
@@ -125,7 +143,8 @@ let questionsLending = [
         answers : [
             {
                 text : "Да, это ж круто)",
-                price : "+10$"
+				price : "+10$",
+				id : "form"
             },
             {
                 text : "Нет, обойдусь(",
@@ -139,7 +158,8 @@ let questionsLending = [
         answers : [
             {
                 text : "Будет, я его очень хочу)",
-                price : "+10$"
+				price : "+10$",
+				id : "slider"
             },
             {
                 text : "Да нет, обойдусь обычным слайдером",
@@ -157,7 +177,8 @@ let questionsLending = [
             },
             {
                 text : "Да, у меня есть очень много информации (>5 экранов)",
-                price : "+35$"
+				price : "+35$",
+				id : "manyscreens"
             }
         ]
     }
@@ -167,16 +188,22 @@ let questionsLending = [
 
 
 //Проверка
-if (next) next.addEventListener('click', ()=>{
-    if (radio[0].checked==true || radio[1].checked==true) {
-		let lending = false
-		if (radio[1].checked==true) lending = true
-        loadFirst(lending)
-    } else if (radio[2].checked==true) {
-        loadSecond()
-    } else if (radio[3].checked==true) {
-        loadThird()
-    } else {
-        errChoose()
-    }
+if (next) next.addEventListener('click', (e)=>{
+	e.preventDefault()
+	if (numberQ == 0) {
+		if (radio[0].checked==true || radio[1].checked==true) {
+			let lending = false
+			if (radio[1].checked==true) lending = true
+			loadFirst(lending)
+			branchQuestions = questionsLending
+		} else if (radio[2].checked==true) {
+			loadSecond()
+		} else if (radio[3].checked==true) {
+			loadThird()
+		} else {
+			errChoose()
+		}
+	} else {
+		nextQuestion(branchQuestions)
+	}
 })
