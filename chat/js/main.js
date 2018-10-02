5523e81e72a51c7c6167dae6356cdf88bca00d67
Vladislav Luahovetsky <vladysymo@mail.ru>
2018-10-02 23:32:45 +0300

    1

--- chat/js/main.js
+++ chat/js/main.js
@@ -1,21 +1,44 @@
-let addMess = (message, my = true)=>{
-    let div = document.createElement('div')
-    div.classList.add('message')
-    if (my) {
-        div.classList.add('my')
-    } else {
-        div.classList.add('nmy')
+class Message {
+    constructor(message, my) {
+        this.elem = document.createElement('div')
+        this.elem.classList.add('message')
+        my ? this.elem.classList.add('my') : this.elem.classList.add('nmy')
+        this.messElem = document.createElement('div')
+        this.messElem.classList.add('msg')
+        this.messElem.innerHTML = message
     }
-    div.innerHTML = message
-    document.querySelector('.chat_list').appendChild(div)
-    document.querySelector('.chat_list').scrollTop = document.querySelector('.chat_list').scrollHeight
+
+    add(elem) {
+        elem = document.querySelector(elem)
+        this.elem.appendChild(this.messElem)
+        elem.appendChild(this.elem)
+    }
 }
 
+let scr = (elem)=>{
+    elem = document.querySelector(elem)
+    elem.scrollTop = elem.scrollHeight
+}
 
+let addMess = (message, my = true)=>{
+    if (message) {
+        let msg = new Message(message, my)
+        msg.add('.chat_list')
+        scr('.chat_list')
+    } else {
+        return false
+    }
+}
 
 document.querySelector('form').addEventListener('submit', (e)=>{
     e.preventDefault()
     let mess = document.querySelector('input').value
     addMess(mess)
     document.querySelector('input').value = ''
+    document.querySelector('input').focus()
+})
+
+window.addEventListener('resize', ()=>{ 
+    let h = document.querySelector('input').getBoundingClientRect().top - 70
+    document.querySelector('.chat_list').style.height = h + 'px'
 })
